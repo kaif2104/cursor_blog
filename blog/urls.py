a9f83ddc94b47d3d98c4c6756a45cf1102c67b6c
kaif2104 <mohammedkaifdudhwala@gmail.com>
2025-08-06 17:44:44 +0530

    final

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
     path('posts/', views.post_list, name='post_list'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),
     path('posts/create/', views.post_create, name='post_create'),
